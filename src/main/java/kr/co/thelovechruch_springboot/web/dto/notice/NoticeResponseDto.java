baffabf6e9958aa2d085d8f42946e8f48f9341b0
dyKim <kdy8982@naver.com>
2023-01-29 00:55:33 +0900

    날짜 출력 형식 yyyy-MM-dd로 나오도록 변경.

--- src/main/java/kr/co/thelovechruch_springboot/web/dto/notice/NoticeResponseDto.java
+++ src/main/java/kr/co/thelovechruch_springboot/web/dto/notice/NoticeResponseDto.java
@@ -21,7 +21,7 @@ public class NoticeResponseDto {
         this.id = entity.getId();
         this.title = entity.getTitle();
         this.author = entity.getAuthor();
-        this.createdDate = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(entity.getCreatedDate());
+        this.createdDate = DateTimeFormatter.ofPattern("yyyy-MM-dd").format(entity.getCreatedDate());
         this.content = entity.getContent();
     }
 }
